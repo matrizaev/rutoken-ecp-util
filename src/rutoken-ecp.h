2010c46b3bcff4a3610c3abc3b85d31687d1e7e9
Viacheslav Matrizaev <matrizaev@gmail.com>
2024-08-04 19:37:48 +0300

    First draft

---
+++ src/rutoken-ecp.h
@@ -0,0 +1,7 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#define PKCS11_LIBRARY_NAME "librtpkcs11ecp.so"
+#define arraysize(a) (sizeof(a) / sizeof(a[0]))
+
+extern uint8_t *sign(void *inData, size_t inputLength, size_t *outputLength, uint8_t *userPIN, size_t userPINLen, uint8_t *keyPairId, size_t keyPairIdLen, size_t slot);
